3f77ae2bf0ea8e4ace2381ec6635d592d7659cfa
serksimper <47192699+serksimper@users.noreply.github.com>
2023-04-06 09:14:09 -0600

    Update modbus-server.js
    allowing chatgpt to build the code and evolve it to make it actually work

--- modbus-server.js
+++ modbus-server.js
@@ -1,17 +1,57 @@
 const net = require('net');
-const { Response } = require('jsmodbus');
+const { Request } = require('jsmodbus');
 
 const server = net.createServer((socket) => {
+	console.log('Client connected');
+
 	// Handle incoming data from clients
 	socket.on('data', (data) => {
 		try {
-			const response = new Response().fromRequest(data);
-			console.log(response);
+			const request = Request.fromBuffer(data);
+			console.log(request);
 
-			// Always send an empty response back
-			socket.write(response.createResponse().toBuffer());
+			switch (request.functionCode) {
+				case Request.FunctionCodes.ReadCoils:
+					// Handle read coils request
+					let response = {
+						unitId: request.unitId,
+						functionCode: request.functionCode,
+						byteCount: 2,
+						payload: Buffer.alloc(2)
+					};
+					response.payload.writeUInt16BE(0b00000001, 0);
+
+					socket.write(Request.buildResponse(response).toBuffer());
+					console.log('Coils read');
+					break;
+
+				case Request.FunctionCodes.WriteSingleCoil:
+					// Handle write single coil request
+					let writeSingleCoilResponse = {
+						unitId: request.unitId,
+						functionCode: request.functionCode,
+						outputAddress: request.outputAddress,
+						outputValue: request.outputValue
+					};
+					socket.write(Request.buildResponse(writeSingleCoilResponse).toBuffer());
+					console.log('Coil written');
+					break;
+
+				default:
+					// Handle unsupported function code
+					console.log(`Unsupported function code: ${request.functionCode}`);
+					let errorResponse = {
+						unitId: request.unitId,
+						functionCode: request.functionCode + 0x80,
+						exceptionCode: 1 // Function code not supported
+					};
+					socket.write(Request.buildResponse(errorResponse).toBuffer());
+					break;
+			}
 		} catch (err) {
-			console.log(err);
+			// Handle invalid requests
+			console.log(`Invalid request: ${err.message}`);
+			socket.write(err.response.toBuffer());
 		}
 	});
 });
