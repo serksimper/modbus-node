d0a270c0e51782497fcb2ea271f9b3633e8dac0d
serksimper <47192699+serksimper@users.noreply.github.com>
2023-04-06 09:27:57 -0600

    Update modbus-client-read-coils.js
    more gpt-4 bizarre code blurbs

--- modbus-client-read-coils.js
+++ modbus-client-read-coils.js
@@ -1,13 +1,95 @@
-const Modbus = require('jsmodbus');
 const net = require('net');
+const { Request } = require('jsmodbus');
 
-const socket = new net.Socket();
-const client = new Modbus.client.TCP(socket, 1);
+const client = new net.Socket();
+const PORT = 502;
+const HOST = 'localhost';
 
-socket.connect(502, '127.0.0.1', () => {
-	console.log(`Opening socket connection to modbus server`);
-	client.readCoils(0, 8, (err, data) => {
-		console.log(data);
-		socket.end();
-	});
+client.connect(PORT, HOST, () => {
+  console.log('Connected to the Modbus server.');
+
+  readCoils(client, 0, 10)
+    .then((response) => {
+      console.log('Read coils response:', response);
+
+      return writeSingleCoil(client, 0, true);
+    })
+    .then((response) => {
+      console.log('Write single coil response:', response);
+
+      return readHoldingRegisters(client, 0, 5);
+    })
+    .then((response) => {
+      console.log('Read holding registers response:', response);
+
+      return writeSingleRegister(client, 0, 12345);
+    })
+    .then((response) => {
+      console.log('Write single register response:', response);
+
+      client.end();
+    })
+    .catch((err) => {
+      console.log('Error:', err.message);
+      client.end();
+    });
 });
+
+client.on('error', (err) => {
+  console.log(`Client error: ${err.message}`);
+});
+
+function readCoils(client, startAddress, quantity) {
+  const request = Request.buildRequest({
+    functionCode: Request.FunctionCodes.ReadCoils,
+    startAddress: startAddress,
+    quantity: quantity
+  });
+
+  return sendRequest(client, request.toBuffer());
+}
+
+function writeSingleCoil(client, outputAddress, outputValue) {
+  const request = Request.buildRequest({
+    functionCode: Request.FunctionCodes.WriteSingleCoil,
+    outputAddress: outputAddress,
+    outputValue: outputValue
+  });
+
+  return sendRequest(client, request.toBuffer());
+}
+
+function readHoldingRegisters(client, startAddress, quantity) {
+  const request = Request.buildRequest({
+    functionCode: Request.FunctionCodes.ReadHoldingRegisters,
+    startAddress: startAddress,
+    quantity: quantity
+  });
+
+  return sendRequest(client, request.toBuffer());
+}
+
+function writeSingleRegister(client, registerAddress, registerValue) {
+  const request = Request.buildRequest({
+    functionCode: Request.FunctionCodes.WriteSingleRegister,
+    registerAddress: registerAddress,
+    registerValue: registerValue
+  });
+
+  return sendRequest(client, request.toBuffer());
+}
+
+function sendRequest(client, requestData) {
+  return new Promise((resolve, reject) => {
+    client.write(requestData);
+
+    client.once('data', (data) => {
+      try {
+        const response = Request.fromBuffer(data);
+        resolve(response);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+}
